70964c086939c2fba854b34aa90ef8825b66c9e0
jasondodds <jason.dodds@gmail.com>
2016-02-22 17:20:28 -0500

    remove all files

--- README.md
+++
@@ -1,5 +0,0 @@
-# PrayingMantis
-DevOps management tool.
-
-This is what we are trying to automate:
-https://docs.google.com/document/d/1BWpWrTJ4Ezw5ZuRLYyxuqIc82WOpDfVwduGSU7DxU0c/edit#heading=h.614o0a9ej0vv
--- spa/project-wizard/app/serializers/build-settings.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
-	attrs: {
-		environments: { embedded: 'always' },
-		repositories: { embedded: 'always' },
-		teamMembers: { embedded: 'always' },
-	}
-});
